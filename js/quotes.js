b1bf245fb908884e5f4cdaae8c531963da58a0ae
Michael Stucki <mstucki51@gmail.com>
2017-07-09 16:09:36 -0600

    replaced quotes.js topics property with tags and made it a list, an array

--- js/quotes.js
+++ js/quotes.js
@@ -2,68 +2,68 @@ var quotes = [
   {quote: "While there's life, there's hope.",
    source: "Cicero",
    citation: "Ad Atticum",
-   topic: "hope"
+   tags: ["hope", "attitude"]
   },
   {quote: "The madman is not the man who has lost his reason. The madman is the man who has lost everything except his reason.",
    source: "G. K. Chesterton",
    citation: "Orthodoxy",
    year: 1909,
-   topic: "reason"
+   tags: ["reason", "insanity"]
   },
   {quote: "It cost God nothing, so far as we know, to create nice things: but to convert rebellious wills cost him crucifixion.",
    source: "C. S. Lewis",
    citation: "Mere Christianity",
    year: 1952,
-   topic: "crucifixion"
+   tags: ["crucifixion", "Christianity", "religion", "sacrifice"]
   },
   {quote: "All that is gold does not glitter, not all those who wander are lost.",
    source: "J. R. R. Tolkien",
    citation: "The Fellowship of the Ring",
    year: 1954,
-   topic: "appearance"
+   tags: ["appearance", "reality", "beauty"]
   },
   {quote: "You ask, what is our policy? I say it is to wage war by land, sea, and air. War with all our might and with all the strength God has given us, and to wage war against a monstrous tyranny never surpassed in the dark and lamentable catalogue of human crime. That is our policy.",
    source: "Winston Churchill",
    citation: "Blood, Sweat, Tears",
    year: 1940,
-   topic: "war"
+   tags: ["war", "determination"]
   },
   {quote: "Numberless are the world's wonders, but none more wonderful than man.",
    source: "Sophocles",
    citation: "Antigone",
-   topic: "humankind"
+   tags: ["mankind", "man", "mystery"]
   },
   {quote: "I have discovered that all human evil comes from this, man's being unable to sit still in a room.",
    source: "Blaise Pascal",
    citation: "Pensees",
    year: 1690,
-   topic: "ennui"
+   tags: ["boredom", "discontent", "solitude"]
   },
   {quote: "Our food should be our medicine. Our medicine should be our food.",
    source: "Hippocrates",
    citation: "",
-   topic: "health"
+   tags: ["health", "food", "medicine"]
   },
   {quote: "The answer, my friend, is blowin' in the wind, the answer is blowin' in the wind.",
    source: "Bob Dylan",
    citation: "Blowin' in the Wind",
    year: 1962,
-   topic: "music"
+   tags: ["music", "wind"]
   },
   {quote: "The prosperity of a fool is a heavy burden to put up with.",
    source: "Aeschylus",
    citation: "",
-   topic: "prosperity"
+   tags: ["prosperity", "fools", "money", "success and failure"]
   },
   {quote: "Old things become new with the passage of time.",
    source: "Nicostratus",
    citation: "",
-   topic: "time"
+   tags: ["old and new", "time"]
   },
   {quote: "An invasion of armies can be resisted, bot not an idea whose time has come.",
    source: "Victor Hugo",
    citation: "Histoire d'un crime",
    year: 1852,
-   topic: "ideas"
+   tags: ["ideas", "time"]
   }
 ];
