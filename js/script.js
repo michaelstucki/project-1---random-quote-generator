cb0248469327009001f3d977e8d85c83625ef666
Michael Stucki <mstucki51@gmail.com>
2017-07-09 15:39:37 -0600

    added random body background-color feature

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 //document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// generate a random number from 1 to 256
+function randomNumber() {
+  return Math.floor(Math.random() * 256) + 1;
+}
+
+// generate a randomColor
+function randomColor() {
+  return "rgb(" + randomNumber() + "," + randomNumber() + "," + randomNumber() + ")";
+}
+
+// change body background-color to a randomly selected color
+document.body.style.backgroundColor = randomColor();
